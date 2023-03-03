c9d9bd062b47effa3a04617c64715b5ca427d0c4
Przemysław Łada <przlada@gmail.com>
2023-03-03 08:58:26 +0100

    Bind socat tunnel to localhost

--- saleor/entrypoint.sh
+++ saleor/entrypoint.sh
@@ -4,7 +4,7 @@
 if [ -v LOCAL_DEV_APP_PORT ]
 then
     echo "Redirecting localhost:$LOCAL_DEV_APP_PORT to host.docker.internal:$LOCAL_DEV_APP_PORT"
-    (socat TCP-LISTEN:$LOCAL_DEV_APP_PORT,reuseaddr,fork TCP:host.docker.internal:$LOCAL_DEV_APP_PORT) &
+    (socat TCP-LISTEN:$LOCAL_DEV_APP_PORT,bind=localhost,reuseaddr,fork TCP:host.docker.internal:$LOCAL_DEV_APP_PORT) &
 fi
 
 # Forward command
