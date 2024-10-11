75b8af6bfbfa3a642b8cdefb0b324c4007b99098
kadewu <krzysztof.waliczek@saleor.io>
2024-10-11 12:51:45 +0200

    force closing connections

--- run-automation.sh
+++ run-automation.sh
@@ -5,7 +5,7 @@ perl -pi -e 's/update_automation_snapshot_staging_saleor_cloud/public/' $SNAPSHO
 # please note that you should not use this password on production services
 DB_URL="postgresql://saleor:saleor@localhost:5432/"
 FULL_DB_URL=$DB_URL"e2e"
-psql $DB_URL -c 'DROP DATABASE IF EXISTS e2e;'
+psql $DB_URL -c 'DROP DATABASE IF EXISTS e2e WITH(FORCE);'
 psql $DB_URL -c 'CREATE DATABASE e2e;' 
 psql $FULL_DB_URL -c 'CREATE EXTENSION IF NOT EXISTS btree_gin; CREATE EXTENSION IF NOT EXISTS pg_trgm;'
 psql $FULL_DB_URL -f $SNAPSHOT
